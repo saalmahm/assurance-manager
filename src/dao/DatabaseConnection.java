0898284a6300d78478bd55096356e4098583b5c3
salma hamdi <saalmahm51@gmail.com>
2025-09-24 11:31:26 +0100

    Ajouter la classe DatabaseConnection pour établir une connexion à PostgreSQL

---
+++ src/dao/DatabaseConnection.java
@@ -0,0 +1,25 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnection {
+    // URL de connexion JDBC
+    private static final String URL = "jdbc:postgresql://localhost:5432/assurance_manager";
+    private static final String USER = "postgres";
+    private static final String PASSWORD = "hamdi";
+
+    public static void main(String[] args) {
+        try {
+            // Établir la connexion
+            Connection conn = DriverManager.getConnection(URL, USER, PASSWORD);
+            System.out.println(" Connexion réussie à PostgreSQL !");
+
+            // Fermer la connexion
+            conn.close();
+        } catch (SQLException e) {
+            System.out.println(" Erreur de connexion : " + e.getMessage());
+        }
+    }
+}
