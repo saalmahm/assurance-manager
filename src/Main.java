ef56ae50c9e658c04e53f8203b895c1c1af51303
salma hamdi <saalmahm51@gmail.com>
2025-09-25 12:00:22 +0100

    Ajouter un menu principal pour gérer les clients et les conseillers

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,26 @@
 import view.ClientView;
+import view.ConseillerView;
+
+import java.util.Scanner;
 
 public class Main {
+
     public static void main(String[] args) {
-        new ClientView().menu();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("1. Gérer Clients");
+        System.out.println("2. Gérer Conseillers");
+        int choix = sc.nextInt();
+        sc.nextLine();
+
+        switch (choix) {
+            case 1:
+                new ClientView().menu();
+                break;
+            case 2:
+                new ConseillerView().menu();
+                break;
+            default:
+                System.out.println("Choix invalide");
+        }
     }
 }
