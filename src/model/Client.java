d3d3a3f6d85df1de030fa747e62e8023b71721a7
salma hamdi <saalmahm51@gmail.com>
2025-09-26 16:39:00 +0100

    Refactor la classe Client pour simplifier les constructeurs, les getters et setters, et améliorer la gestion des conseillers

--- src/model/Client.java
+++ src/model/Client.java
@@ -8,60 +8,42 @@ public class Client extends Personne {
     private Conseiller conseiller;        // lien avec le conseiller
     private Map<Integer, Contrat> contrats; // contrats du client
 
-    // Constructeur vide
-    public Client() {
-        this.contrats = new HashMap<>();
-    }
-
-    // Constructeur avec infos de base
+    public Client() { this.contrats = new HashMap<>(); }
     public Client(String nom, String prenom, String email, Conseiller conseiller) {
         super(nom, prenom, email);
         this.conseiller = conseiller;
         this.contrats = new HashMap<>();
     }
 
-    // Getters et Setters
-    public int getId() {
-        return id;
-    }
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
 
-    public void setId(int id) {
-        this.id = id;
-    }
+    public Conseiller getConseiller() { return conseiller; }
+    public void setConseiller(Conseiller conseiller) { this.conseiller = conseiller; }
 
-    public Conseiller getConseiller() {
-        return conseiller;
-    }
+    public Map<Integer, Contrat> getContrats() { return contrats; }
+    public void setContrats(Map<Integer, Contrat> contrats) { this.contrats = contrats; }
 
-    public void setConseiller(Conseiller conseiller) {
-        this.conseiller = conseiller;
-    }
-
-    public Map<Integer, Contrat> getContrats() {
-        return contrats;
-    }
-
-    public void setContrats(Map<Integer, Contrat> contrats) {
-        this.contrats = contrats;
-    }
-
-    // Ajouter un contrat au client
     public void ajouterContrat(Contrat contrat) {
-        if (contrat != null) {
-            this.contrats.put(contrat.getId(), contrat);
+        if (contrat != null) this.contrats.put(contrat.getId(), contrat);
+    }
+
+    public Integer getConseillerId() { return conseiller != null ? conseiller.getId() : null; }
+
+    public void setConseillerId(Integer id) {
+        if (id != null && id > 0) {
+            if (this.conseiller == null) this.conseiller = new Conseiller();
+            this.conseiller.setId(id);
+        } else {
+            this.conseiller = null;
         }
     }
 
-    // Méthode utilitaire pour récupérer l'id du conseiller (pour DAO)
-    public int getConseillerId() {
-        return conseiller != null ? conseiller.getId() : 0;
-    }
 
-    // Méthode utilitaire pour lier un conseiller via son ID (pratique pour DAO)
-    public void setConseillerId(int id) {
-        if (this.conseiller == null) {
-            this.conseiller = new Conseiller();
-        }
-        this.conseiller.setId(id);
+    @Override
+    public String toString() {
+        return "Client{" + "id=" + id + ", nom='" + nom + '\'' +
+                ", prenom='" + prenom + '\'' + ", email='" + email + '\'' +
+                ", conseillerId=" + getConseillerId() + '}';
     }
 }
