8aece7b280ca58947a80fd25b3aa29b01d9da193
salma hamdi <saalmahm51@gmail.com>
2025-09-26 16:39:39 +0100

    Améliorer la classe Conseiller en simplifiant le code et en ajoutant une méthode toString pour afficher les informations des conseillers et de leurs clients

--- src/model/Conseiller.java
+++ src/model/Conseiller.java
@@ -2,32 +2,41 @@ package model;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.stream.Collectors;
+
 
 public class Conseiller extends Personne {
     private int id;
     private Map<Integer, Client> clients;
 
-    public Conseiller() {
-        this.clients = new HashMap<>();
-    }
-
+    public Conseiller() { this.clients = new HashMap<>(); }
     public Conseiller(String nom, String prenom, String email) {
         super(nom, prenom, email);
         this.clients = new HashMap<>();
     }
 
-    // Getters et Setters
     public int getId() { return id; }
     public void setId(int id) { this.id = id; }
 
     public Map<Integer, Client> getClients() { return clients; }
     public void setClients(Map<Integer, Client> clients) { this.clients = clients; }
 
-    public void ajouterClient(Client client) {
-        this.clients.put(client.getId(), client);
-    }
+    public void ajouterClient(Client client) { this.clients.put(client.getId(), client); }
+    public void supprimerClient(int clientId) { this.clients.remove(clientId); }
 
-    public void supprimerClient(int clientId) {
-        this.clients.remove(clientId);
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Conseiller : ").append(nom).append(" ").append(prenom).append("\n");
+        sb.append("Email : ").append(email).append("\n");
+        sb.append("Clients : ");
+        if (clients.isEmpty()) {
+            sb.append("aucun client");
+        } else {
+            sb.append(String.join(", ", clients.values().stream()
+                    .map(c -> c.getNom() + " " + c.getPrenom())
+                    .collect(Collectors.toList()))); // <-- ici, Java 8 compatible
+        }
+        return sb.toString();
     }
 }
