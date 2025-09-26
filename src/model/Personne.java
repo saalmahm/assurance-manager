806580492929da89df9552c9d2cc1c1dfd223af1
salma hamdi <saalmahm51@gmail.com>
2025-09-26 16:38:40 +0100

    Simplifier la classe Personne en supprimant les espaces inutiles et en ajoutant un retour à la ligne à la fin du fichier

--- src/model/Personne.java
+++ src/model/Personne.java
@@ -6,20 +6,16 @@ public abstract class Personne {
     protected String email;
 
     public Personne() {}
-
     public Personne(String nom, String prenom, String email) {
         this.nom = nom;
         this.prenom = prenom;
         this.email = email;
     }
 
-    // Getters et Setters
     public String getNom() { return nom; }
     public void setNom(String nom) { this.nom = nom; }
-
     public String getPrenom() { return prenom; }
     public void setPrenom(String prenom) { this.prenom = prenom; }
-
     public String getEmail() { return email; }
     public void setEmail(String email) { this.email = email; }
 }
