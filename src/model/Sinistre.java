27d08cd23bc892a2f8ac6b3275c31e2ff1e344ea
salma hamdi <saalmahm51@gmail.com>
2025-09-27 21:32:37 +0100

    Refactor la classe Sinistre pour simplifier la déclaration des attributs et améliorer la méthode toString

--- src/model/Sinistre.java
+++ src/model/Sinistre.java
@@ -3,85 +3,38 @@ package model;
 import java.time.LocalDateTime;
 
 public class Sinistre {
+    private int id;
+    private TypeSinistre typeSinistre;
+    private LocalDateTime dateTime;
+    private double cout;
     private String description;
-    private LocalDateTime dateOccurrence;
-    private String lieu;
-    private double montantDommage;
-    private String statut;
-    private String numeroSinistre;
+    private int contratId;
 
-    // Constructors
-    public Sinistre() {
-    }
-
-    public Sinistre(String numeroSinistre, String description, LocalDateTime dateOccurrence,
-                    String lieu, double montantDommage, String statut) {
-        this.numeroSinistre = numeroSinistre;
+    public Sinistre(int id, TypeSinistre typeSinistre, LocalDateTime dateTime, double cout, String description, int contratId) {
+        this.id = id;
+        this.typeSinistre = typeSinistre;
+        this.dateTime = dateTime;
+        this.cout = cout;
         this.description = description;
-        this.dateOccurrence = dateOccurrence;
-        this.lieu = lieu;
-        this.montantDommage = montantDommage;
-        this.statut = statut;
+        this.contratId = contratId;
     }
 
-    // Getters and Setters
-    public String getNumeroSinistre() {
-        return numeroSinistre;
-    }
-
-    public void setNumeroSinistre(String numeroSinistre) {
-        this.numeroSinistre = numeroSinistre;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public LocalDateTime getDateOccurrence() {
-        return dateOccurrence;
-    }
-
-    public void setDateOccurrence(LocalDateTime dateOccurrence) {
-        this.dateOccurrence = dateOccurrence;
-    }
-
-    public String getLieu() {
-        return lieu;
-    }
-
-    public void setLieu(String lieu) {
-        this.lieu = lieu;
-    }
-
-    public double getMontantDommage() {
-        return montantDommage;
-    }
-
-    public void setMontantDommage(double montantDommage) {
-        this.montantDommage = montantDommage;
-    }
-
-    public String getStatut() {
-        return statut;
-    }
-
-    public void setStatut(String statut) {
-        this.statut = statut;
-    }
+    // Getters et Setters
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
+    public TypeSinistre getTypeSinistre() { return typeSinistre; }
+    public void setTypeSinistre(TypeSinistre typeSinistre) { this.typeSinistre = typeSinistre; }
+    public LocalDateTime getDateTime() { return dateTime; }
+    public void setDateTime(LocalDateTime dateTime) { this.dateTime = dateTime; }
+    public double getCout() { return cout; }
+    public void setCout(double cout) { this.cout = cout; }
+    public String getDescription() { return description; }
+    public void setDescription(String description) { this.description = description; }
+    public int getContratId() { return contratId; }
+    public void setContratId(int contratId) { this.contratId = contratId; }
 
     @Override
     public String toString() {
-        return "Sinistre{" +
-                "numeroSinistre='" + numeroSinistre + '\'' +
-                ", description='" + description + '\'' +
-                ", dateOccurrence=" + dateOccurrence +
-                ", lieu='" + lieu + '\'' +
-                ", montantDommage=" + montantDommage +
-                ", statut='" + statut + '\'' +
-                '}';
+        return "Sinistre{id=" + id + ", type=" + typeSinistre + ", date=" + dateTime + ", cout=" + cout + ", contratId=" + contratId + "}";
     }
 }
