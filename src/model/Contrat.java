0e13fa24d92bd5c5a93d18d6687b84e41dec51e6
salma hamdi <saalmahm51@gmail.com>
2025-09-27 21:32:08 +0100

    Refactor la classe Contrat pour remplacer l'objet Client par un identifiant client et améliorer la méthode toString

--- src/model/Contrat.java
+++ src/model/Contrat.java
@@ -7,30 +7,30 @@ public class Contrat {
     private TypeContrat typeContrat;
     private LocalDate dateDebut;
     private LocalDate dateFin;
-    private Client client;
+    private int clientId;
 
-    public Contrat() {}
-
-    public Contrat(TypeContrat typeContrat, LocalDate dateDebut, LocalDate dateFin, Client client) {
+    public Contrat(int id, TypeContrat typeContrat, LocalDate dateDebut, LocalDate dateFin, int clientId) {
+        this.id = id;
         this.typeContrat = typeContrat;
         this.dateDebut = dateDebut;
         this.dateFin = dateFin;
-        this.client = client;
+        this.clientId = clientId;
     }
 
     // Getters et Setters
     public int getId() { return id; }
     public void setId(int id) { this.id = id; }
-
     public TypeContrat getTypeContrat() { return typeContrat; }
     public void setTypeContrat(TypeContrat typeContrat) { this.typeContrat = typeContrat; }
-
     public LocalDate getDateDebut() { return dateDebut; }
     public void setDateDebut(LocalDate dateDebut) { this.dateDebut = dateDebut; }
-
     public LocalDate getDateFin() { return dateFin; }
     public void setDateFin(LocalDate dateFin) { this.dateFin = dateFin; }
+    public int getClientId() { return clientId; }
+    public void setClientId(int clientId) { this.clientId = clientId; }
 
-    public Client getClient() { return client; }
-    public void setClient(Client client) { this.client = client; }
+    @Override
+    public String toString() {
+        return "Contrat{id=" + id + ", type=" + typeContrat + ", debut=" + dateDebut + ", fin=" + dateFin + ", clientId=" + clientId + "}";
+    }
 }
