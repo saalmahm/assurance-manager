151b4ecb7853f25524a9913e0323143809ceabee
salma hamdi <saalmahm51@gmail.com>
2025-09-27 21:31:44 +0100

    Refactor la classe Conseiller pour simplifier la gestion des attributs et améliorer la méthode toString

--- src/model/Conseiller.java
+++ src/model/Conseiller.java
@@ -1,42 +1,18 @@
 package model;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.stream.Collectors;
-
-
-public class Conseiller extends Personne {
+public class Conseiller extends Person {
     private int id;
-    private Map<Integer, Client> clients;
 
-    public Conseiller() { this.clients = new HashMap<>(); }
-    public Conseiller(String nom, String prenom, String email) {
+    public Conseiller(int id, String nom, String prenom, String email) {
         super(nom, prenom, email);
-        this.clients = new HashMap<>();
+        this.id = id;
     }
 
     public int getId() { return id; }
     public void setId(int id) { this.id = id; }
 
-    public Map<Integer, Client> getClients() { return clients; }
-    public void setClients(Map<Integer, Client> clients) { this.clients = clients; }
-
-    public void ajouterClient(Client client) { this.clients.put(client.getId(), client); }
-    public void supprimerClient(int clientId) { this.clients.remove(clientId); }
-
     @Override
     public String toString() {
-        StringBuilder sb = new StringBuilder();
-        sb.append("Conseiller : ").append(nom).append(" ").append(prenom).append("\n");
-        sb.append("Email : ").append(email).append("\n");
-        sb.append("Clients : ");
-        if (clients.isEmpty()) {
-            sb.append("aucun client");
-        } else {
-            sb.append(String.join(", ", clients.values().stream()
-                    .map(c -> c.getNom() + " " + c.getPrenom())
-                    .collect(Collectors.toList()))); // <-- ici, Java 8 compatible
-        }
-        return sb.toString();
+        return "Conseiller{id=" + id + ", nom='" + getNom() + "', prenom='" + getPrenom() + "'}";
     }
 }
