53cb9c4398011b828e6a13b0ab42c745cfb2f944
salma hamdi <saalmahm51@gmail.com>
2025-09-27 21:30:59 +0100

    Refactor la classe Client pour simplifier la gestion des conseillers et améliorer la méthode toString

--- src/model/Client.java
+++ src/model/Client.java
@@ -1,49 +1,22 @@
 package model;
 
-import java.util.HashMap;
-import java.util.Map;
-
-public class Client extends Personne {
+public class Client extends Person {
     private int id;
-    private Conseiller conseiller;        // lien avec le conseiller
-    private Map<Integer, Contrat> contrats; // contrats du client
+    private int conseillerId;
 
-    public Client() { this.contrats = new HashMap<>(); }
-    public Client(String nom, String prenom, String email, Conseiller conseiller) {
+    public Client(int id, String nom, String prenom, String email, int conseillerId) {
         super(nom, prenom, email);
-        this.conseiller = conseiller;
-        this.contrats = new HashMap<>();
+        this.id = id;
+        this.conseillerId = conseillerId;
     }
 
     public int getId() { return id; }
     public void setId(int id) { this.id = id; }
-
-    public Conseiller getConseiller() { return conseiller; }
-    public void setConseiller(Conseiller conseiller) { this.conseiller = conseiller; }
-
-    public Map<Integer, Contrat> getContrats() { return contrats; }
-    public void setContrats(Map<Integer, Contrat> contrats) { this.contrats = contrats; }
-
-    public void ajouterContrat(Contrat contrat) {
-        if (contrat != null) this.contrats.put(contrat.getId(), contrat);
-    }
-
-    public Integer getConseillerId() { return conseiller != null ? conseiller.getId() : null; }
-
-    public void setConseillerId(Integer id) {
-        if (id != null && id > 0) {
-            if (this.conseiller == null) this.conseiller = new Conseiller();
-            this.conseiller.setId(id);
-        } else {
-            this.conseiller = null;
-        }
-    }
-
+    public int getConseillerId() { return conseillerId; }
+    public void setConseillerId(int conseillerId) { this.conseillerId = conseillerId; }
 
     @Override
     public String toString() {
-        return "Client{" + "id=" + id + ", nom='" + nom + '\'' +
-                ", prenom='" + prenom + '\'' + ", email='" + email + '\'' +
-                ", conseillerId=" + getConseillerId() + '}';
+        return "Client{id=" + id + ", nom='" + getNom() + "', prenom='" + getPrenom() + "', conseillerId=" + conseillerId + "}";
     }
 }
